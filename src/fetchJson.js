61bbfce6a7f2553fc32a94608dfc1e65515e9854
Drew Bolles <andrew.bolles@ncr.com>
2018-10-26 13:23:57 -0400

    Adjust fetchJson function

--- src/fetchJson.js
+++ src/fetchJson.js
@@ -1,8 +1,19 @@
 import 'isomorphic-unfetch';
 
+const processResponse = res => {
+  if (res.status === 200) {
+    try {
+      const json = res.json();
+      return json;
+    } catch (e) {
+      console.log('Response was not valid JSON', e);
+    }
+  }
+};
+
 function fetchJson(url, opts) {
   return fetch(url, opts)
-    .then(res => res.json())
+    .then(processResponse)
     .catch(e => console.log('Fetch failed', e));
 }
 
